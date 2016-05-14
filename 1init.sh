123d7f6c10963535f6e1d3fbac0e0506e707ad78
FKolenko <max-kon@yandex.ru>
2016-05-14 13:13:52 +0300

    *

---
+++ 1init.sh
@@ -0,0 +1,4 @@
+﻿sudo ﻿ln -s /home/fil/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/fil/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
